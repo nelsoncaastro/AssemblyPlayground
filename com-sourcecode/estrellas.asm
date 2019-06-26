d766843e63be3963461a2b04cc8ececa263f0ca9
Unknown <00043516@uca.edu.sv>
2019-06-26 01:36:10 -0600

    Added FPU instructions

--- com-sourcecode/estrellas.asm
+++ com-sourcecode/estrellas.asm
@@ -1,26 +1,48 @@
-org 100h
 ;**************************************************
 ;Este ejemplo NO funciona
 ;**************************************************
+
+org 100h
+
+section .bss
+
+penddividendo:	resd 1
+penddivisor:	resd 1
+pendiente: 	resd 1
+yresultante: 	resd 1
+extra: 		resd 1
+
+section .data
+
+x1:	dd 	188.0
+y1:	dd	166.0
+x2:	dd   	200.0 
+y2:	dd	132.0
+
 section .text
 
-start:
+global	_start
+
+_start:
 ;======= Princial 
-;modo video
+
+	call iniciarModoVideo
+	finit
+	call bandera
+	call espera
+	call fin
+;======= Subrutinas
+
+iniciarModoVideo:
 	mov ah, 00h
 	mov al, 12h
 	int 10h
-
-call bandera
-call espera
-call fin
-;======= Subrutinas
+	ret
 
 bandera:
-	;call canvas
+	call canvas
 	;call cuadroAzul
 	;call cuadroRojo
-	;call puntosIniciales
 	call estrellaVerde
 	ret
 
@@ -63,88 +85,49 @@ sigcrojo:	call pixelRojo
 	jne sigcrojo
 	ret
 
-puntosIniciales:
-
-    ;coordenadas iniciales
-    ;mov byte [0500h], 00h
-    ;mov byte [0201h], 00h
-    
-	mov si, 188d
-    mov [0500h], si ; x del primer punto
-    
-    ;mov byte [0502h], 00h
-    ;mov byte [0203h], 00h
-    
-	mov si, 166d
-    mov [0502h], si ; y del primer punto
-    
-    ;coordenadas finales
-    ;mov byte [0504h], 00h
-    ;mov byte [0205h], 00h 
-    
-	mov si, 200d
-    mov [0504h], si ; x del segundo punto
-    
-    ;mov byte [0506h], 00h
-    ;mov byte [0207h], 00h
-    
-	mov si, 132d
-    mov [0506h], si ; y del segundo punto
-    
-    ret 
-
 estrellaVerde:
 ;punto inical
 ;m y b
-	mov si, [0500h]
-	mov cx, si	
-	mov si, [0502h]
-	mov dx, si
+	mov cx, x1
+	mov dx, y1
 	call encontrarbp
-sigeazul:	call encontrardx
+sigeazul:call encontrardx
 	call pixelVerde
 	inc cx
-	mov si, [0504h]
+	mov si, x2
 	cmp cx, si
 	jne sigeazul
-	ret      
+	ret
 
 encontrarbp: ;Encontramos la pendiente de dicha linea recta
-	mov bp , [0506h]
-    mov bx, bp ; y_2
-	mov bp, [0502h]
-    mov si, bp ; y_1
-    
-    sub bx, si ; y_2 - y_1 
-    mov [0508h], bx
-    
-	mov bp, [0504h]
-    mov bx, bp ; x_2
-	mov bp, [0500h]
-    mov si, bp ; x_1
-    
-    sub bx, si ; x_2 - x_1 
-    mov [050Ah], bx 
-    
-	mov bp, [0508h]
-    mov ax, bp ;dividendo 
-	mov bp, [050Ah]
-    mov bx, bp;divisor
-    
-    idiv bx ; y_2 - y_1 / x_2 - x_1 
-    
-    mov bp, ax       
-    ret
+	fld dword [y2] ;y_2
+	fld dword [y1] ; y_1
+	fsub ; y_2 - y_1 
+	fstp dword [penddividendo]
+
+	fld dword [x2]; x_2
+	fld dword [x1]; x_1
+	fsub
+	fstp dword [penddivisor]
+
+	fld dword [penddividendo]
+	fld dword [penddivisor]
+	fdiv
+	fstp dword [pendiente]
+
+	ret
 
 encontrardx:
-	;mov dx,0d
-	mov ax, cx
-	mov si, [0500h] ; posición de cx inicial
-	sub ax, si 
-	imul bp
-	mov si, [0502h] ; posición de dx inicial
-	add ax, si
-	mov dx, ax
+	mov [extra], cx
+	fld dword [extra] 
+	fld dword [x1]
+	fsub
+	fld dword [pendiente]	
+	fmul
+	fld dword [y1]
+	fadd
+	fstp dword [yresultante]
+	mov dx, [yresultante]
 	ret
 
 pixelRojo:
